84ff9ab96d86fce677d419ae0c2dc24e7ca3e358
Lior Rabinovich <lior.px@gmail.com>
2020-05-12 23:01:14 +0300

    asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -14,27 +14,29 @@ export default {
   components: {
     PostList
   },
-  data() {
-    return {
-      loaddedPosts: [
-        {
-          id: '1',
-          title: "First Post",
-          previewText: "This id out first post",
-          thumbnail:
-            "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-          content: "Content of the post"
-        },
-        {
-          id: '2',
-          title: "First Post",
-          previewText: "This id out first post",
-          thumbnail:
-            "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-          content: "Content of the post"
-        }
-      ]
-    };
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        loaddedPosts: [
+          {
+            id: "1",
+            title: "First Post",
+            previewText: "This id out first post",
+            thumbnail:
+              "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+            content: "Content of the post"
+          },
+          {
+            id: "2",
+            title: "First Post",
+            previewText: "This id out first post",
+            thumbnail:
+              "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+            content: "Content of the post"
+          }
+        ]
+      });
+    }, 1500);
   }
 };
 </script>
